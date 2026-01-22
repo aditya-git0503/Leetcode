79f0cc83d7e311bd2b57839eec6253ca6fe9d0fd
aditya-git0503 <aditya.g24@iiits.in>
2026-01-22 18:30:04 +0530

    Time: 7 ms (6.72%), Space: 97.4 MB (59.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,13 +4,11 @@ public:
         int profit = 0;
         int n = prices.size();
         
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                int diff = prices[j] - prices[i];
-                if(diff > profit){
-                    profit = diff;
-                }
-            }
+        int mini = prices[0];
+        for(int i=1; i<n; i++){
+            int cost = prices[i] - mini;
+            profit = max(profit,cost);
+            mini = min(mini,prices[i]);
         }
         return profit;
     }
