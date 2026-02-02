4a0ac656aa42f66487984b9e76b392b79697841b
aditya-git0503 <aditya.g24@iiits.in>
2026-02-02 18:35:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.33%) - LeetHub

--- 3827-count-monobit-integers/3827-count-monobit-integers.cpp
+++ 3827-count-monobit-integers/3827-count-monobit-integers.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int countMonobit(int n) {
-        set<int> st;
-        st.insert(0);
-        for(int i=1; i<=9; i++){
-            int d = pow(2,i)-1;
-            st.insert(d);
-        }
+        // set<int> st;
+        // st.insert(0);
+        // for(int i=1; i<=9; i++){
+        //     int d = pow(2,i)-1;
+        //     st.insert(d);
+        // }
 
-        int ans = 0;
-        for(int i=0; i<=n; i++){
-            if(st.find(i) != st.end()){
+        // int ans = 0;
+        // for(int i=0; i<=n; i++){
+        //     if(st.find(i) != st.end()){
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        if(n == 0 || n == 1){
+            return (n+1);
+        }
+        int ans = 2;
+        for(int i=2; i<=9; i++){
+            if(n >= (pow(2,i)-1)){
                 ans++;
             }
         }
