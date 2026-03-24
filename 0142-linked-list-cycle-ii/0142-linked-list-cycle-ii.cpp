985678e4098d48f7839d00f22ddaac30c39c04e9
aditya-git0503 <aditya.g24@iiits.in>
2026-03-24 20:45:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,20 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        map<ListNode*, int> mpp;
-        ListNode* temp = head;
-        while(temp){
-            if(mpp.find(temp) != mpp.end()){
-                return temp;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow && fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-            mpp[temp] = 1;
-            temp = temp->next;
-        } 
+        }
         return nullptr;
     }
 };
