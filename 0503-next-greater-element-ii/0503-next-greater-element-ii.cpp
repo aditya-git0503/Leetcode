0b21637b89ca43b799f805635538a6651ddcacbf
aditya-git0503 <aditya.g24@iiits.in>
2026-04-07 22:16:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,14 +5,6 @@ public:
         vector<int> ans(n, -1);
         stack<int> st;
 
-        // for(int i=0; i<n-1; i++){
-        //     if(nums[i] > nums[n-1]){
-        //         ans[n-1] = nums[i];
-        //         break;
-        //     }
-        // }
-        // st.push(nums[n-1]);
-
         for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && st.top() <= nums[i]) {
                 st.pop();
