32bb3e41a2cbd0a846d6f4c55d2e0facdf50fa77
aditya-git0503 <aditya.g24@iiits.in>
2026-04-08 09:05:16 +0530

    Time: 0 ms (100%), Space: 22.1 MB (57.71%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& s) {
+        vector<int> ans;
+        stack<int> st;
+        
+        for(int i : s){
+            if(i > 0){
+                st.push(i);
+            }
+            else{
+                while(!st.empty() && st.top() > 0 && st.top() < abs(i)){
+                    st.pop();
+                }
+                if(!st.empty() && st.top() == abs(i)){
+                    st.pop();
+                }
+                else if(st.empty() || st.top() < 0){
+                    st.push(i);
+                }
+            }
+        }
+
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
