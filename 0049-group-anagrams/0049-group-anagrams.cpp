d1aeec7178088a96a5500174e6076659e4043e58
aditya-git0503 <aditya.g24@iiits.in>
2026-04-16 21:56:58 +0530

    Time: 15 ms (69.8%), Space: 26.3 MB (30.02%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+        vector<vector<string>> ans;
+
+        for(string s : strs){
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            mpp[temp].push_back(s);
+        }
+
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
