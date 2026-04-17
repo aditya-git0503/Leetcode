0cb740976accfb5b1dafdc0f2efb24fe71c66237
aditya-git0503 <aditya.g24@iiits.in>
2026-04-17 12:26:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,13 +7,14 @@ public:
         if(n == 1)
             return nums[0];
     
-        int dp[n];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int a = nums[0];
+        int b = max(nums[0], nums[1]);
 
         for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+            int temp = max(b, a + nums[i]);
+            a = b;
+            b = temp;
         }
-        return dp[n-1];
+        return b;
     }
 };
