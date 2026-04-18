56c977afde0f30892d80991cf4708a2749ac628d
aditya-git0503 <aditya.g24@iiits.in>
2026-04-18 18:26:32 +0530

    Time: 0 ms (100%), Space: 10.4 MB (61.43%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int func(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+
+        int a = nums[0];
+        int b = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int temp = max(b, a + nums[i]);
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int> temp1, temp2;
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0)
+                temp1.push_back(nums[i]);
+            if (i != n - 1)
+                temp2.push_back(nums[i]);
+        }
+
+        return max(func(temp1), func(temp2));
+    }
+};
