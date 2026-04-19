6ddb6806c4420dbb0ca2b0900ab6929463cc0918
aditya-git0503 <aditya.g24@iiits.in>
2026-04-19 09:51:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -3,20 +3,18 @@ public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
+        int i = 0, j = 0;
         int ans = 0;
 
-        for(int i=0; i<n1; i++){
-            int l = i;
-            int h = n2-1;
-            while(l <= h){
-                int mid = l + (h - l)/2;
-                if(nums1[i] <= nums2[mid]){
-                    ans = max(ans, mid-i);
-                    l = mid + 1;
-                }
-                else{
-                    h = mid - 1;
-                }
+        while(i < n1 && j <  n2){
+            if(nums1[i] <= nums2[j]){
+                ans = max(ans, j-i);
+                j++;
+            }
+            else{
+                i++;
+                if(i > j)
+                    j = i;
             }
         }
         return ans;
