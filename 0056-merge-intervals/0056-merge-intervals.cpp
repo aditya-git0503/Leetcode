c75eaaff576ccec484d8f7093fed330ada89bff8
Aditya Gupta <aditya.g24@iiits.in>
2026-06-10 00:06:55 +0530

    Time: 5 ms (52.2%), Space: 24 MB (31.5%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        int n = intervals.size();
+
+        if (n == 1) {
+            return intervals;
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<int> curr = intervals[0];
+
+        for (int i = 1; i < n; i++) {
+            if (curr[1] >= intervals[i][0]) {
+                curr[1] = max(curr[1], intervals[i][1]);
+            } else {
+                ans.push_back(curr);
+                curr = intervals[i];
+            }
+        }
+        ans.push_back(curr);
+        return ans;
+    }
+};
