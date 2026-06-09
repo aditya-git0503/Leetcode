9618c9308b23f6609c3104b425e7ea5fd332b653
Aditya Gupta <aditya.g24@iiits.in>
2026-06-09 11:34:45 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.12%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,53 @@
 class Solution {
 public:
 
-    int lb(vector<int>& nums, int target){
-        int n = nums.size();
+    int f(vector<int>& nums, int target, int n){
+        int first = -1;
         int low = 0;
         int high = n-1;
-        int ans = n;
 
         while(low <= high){
-            int mid = high + (low-high)/2;
-            if(nums[mid] >= target){
+            int mid = high + (low - high)/2;
+            if(nums[mid] == target){
+                first = mid;
                 high = mid - 1;
-                ans = mid;
             }
-            else{
+            else if(nums[mid] < target){
                 low = mid + 1;
             }
+            else{
+                high = mid - 1;
+            }
         }
-        return ans;
+        return first;
     }
 
-    int ub(vector<int>& nums, int target){
-        int n = nums.size();
+    int l(vector<int>& nums, int n, int target){
         int low = 0;
         int high = n-1;
-        int ans = n;
+        int last = -1;
 
         while(low <= high){
-            int mid = high + (low-high)/2;
-            if(nums[mid] > target){
-                high = mid - 1;
-                ans = mid;
-            }
-            else{
+            int mid = high + (low - high)/2;
+            if(nums[mid] == target){
+                last = mid;
                 low = mid + 1;
             }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
         }
-        return ans;
+        return last;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = lb(nums, target);
+        int first = -1;
         int n = nums.size();
-        if(l == n || nums[l] != target){
-            return {-1, -1};
-        }
-
-        else{
-            int u = ub(nums, target);
-            return {l, u-1};
-        }
+        int a1 = f(nums, target, n);
+        int a2 = l(nums, n, target);
+        return {a1,a2};
     }
 };
