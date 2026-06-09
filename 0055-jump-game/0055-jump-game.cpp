f0e0af2b523f77cbc96207bb07412b5eed098d68
Aditya Gupta <aditya.g24@iiits.in>
2026-06-09 17:15:21 +0530

    Time: 4 ms (22.78%), Space: 52.3 MB (82.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int farthest = 0;
         int n = nums.size();
-        vector<bool> dp(n, false);
-        dp[n-1] = true;
 
-        for(int i=n-2; i>=0; i--){
-            for(int j=1; j<= nums[i] && j+i < n; j++){
-                if(dp[i+j]){
-                    dp[i] = true;
-                    break;
-                }
+        for(int i=0; i<n; i++){
+            if(i > farthest){
+                return false;
+            }
+            farthest = max(farthest, i + nums[i]);
+            if(farthest >= n-1){
+                return true;
             }
         }
-        return dp[0];
+        return true;
     }
 };
