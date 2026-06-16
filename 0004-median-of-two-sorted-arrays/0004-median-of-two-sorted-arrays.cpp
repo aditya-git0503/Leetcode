9e651b0270ae7efe8319f80506f83d9c563012a5
Aditya Gupta <aditya.g24@iiits.in>
2026-06-17 00:43:27 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.16%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int l = 0, r = 0;
-        int n1 = nums1.size(), n2 = nums2.size();
-        vector<int> final;
+        if (nums1.size() > nums2.size())
+            swap(nums1, nums2);
 
-        while (l < n1 && r < n2) {
-            if (nums1[l] <= nums2[r]) {
-                final.push_back(nums1[l++]);
+        int n1 = nums1.size(), n2 = nums2.size();
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (n1 + n2) / 2 - i;
+
+            int nums1_left = (i == 0) ? INT_MIN : nums1[i - 1];
+            int nums1_right = (i == n1) ? INT_MAX : nums1[i];
+            int nums2_left = (j == 0) ? INT_MIN : nums2[j - 1];
+            int nums2_right = (j == n2) ? INT_MAX : nums2[j];
+
+            if (nums1_left <= nums2_right && nums2_left <= nums1_right) {
+                if ((n1 + n2) % 2 == 1) {
+                    return min(nums1_right, nums2_right);
+                } else {
+                    return (max(nums1_left, nums2_left) +
+                            min(nums1_right, nums2_right)) /
+                           2.0;
+                }
+            } else if (nums1_left > nums2_right) {
+                high = i - 1;
             } else {
-                final.push_back(nums2[r++]);
+                low = i + 1;
             }
         }
-
-        while (l < n1)
-            final.push_back(nums1[l++]);
-        while (r < n2)
-            final.push_back(nums2[r++]);
-
-        int n3 = final.size();
-        if (n3 & 1) {
-            return (double)(final[n3 / 2]);
-        } else {
-            return (final[n3 / 2 - 1] + final[n3 / 2]) / 2.0;
-        }
+        return 0.0;
     }
 };
